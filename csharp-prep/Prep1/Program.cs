7ff7830b7201acc5e309da42b9254bb83e6373e7
keneze.nwangele@gmail.com <you@example.com>
2023-01-18 17:47:16 +0100

    Updated name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask the user for their first name.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        
+        // Ask the user for their last name.
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine();
+        
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
