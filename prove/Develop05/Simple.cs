c7b4d7b6a7c75bd950c545891d769a71db8a11dd
keneze.nwangele@gmail.com <you@example.com>
2023-04-06 11:33:31 +0100

    update

--- prove/Develop05/Simple.cs
+++ prove/Develop05/Simple.cs
@@ -1,31 +1,26 @@
 using System;
 
-public class Simple : Goals
+public class SimpleGoal : Goal
 {
-    private string _goal;
-    private string _description;
-  
-    bool _isComplete = false;
-
-    public string StartSimpleGoals() {
-        Questions();
-        return CreateSentence(_goal, _description, _isComplete);
-    }
-    protected override void Questions() {
-        Console.Write("What is the name of you goal? ");
-        string goal = Console.ReadLine();
-        _goal = goal;
-        Console.Write( "What is a short description of it? ");
-        string description = Console.ReadLine();
-        _description = description;
-        Console.Write("What is the amount of points associated with this goal? ");
-        string strPoints = Console.ReadLine();
-        _points = int.Parse(strPoints);
-    }
-
-
-    public override string CreateFileStr()
+    public SimpleGoal()
     {
-        return $"SimpleGoal:{_goal}<>{_description}<>{_points}<>{_isComplete}";
+
     }
+
+    public SimpleGoal(string goalName, string description, int points, bool isComplete, string type = "SimpleGoal"):
+    base (type, goalName, description, points, isComplete)
+    {
+
+    }
+
+    public override string PrintList()
+    {
+        return base.PrintList();
+    }
+
+    public override string AddToFileList() 
+    {
+        return base.AddToFileList();
+    }
+
 }
